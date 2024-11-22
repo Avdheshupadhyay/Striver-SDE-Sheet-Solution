90f508796e408994ab10cb9b3d653ff41cabc318
Avdhesh Upadhyay <142656855+Avdheshupadhyay@users.noreply.github.com>
2024-11-22 18:11:58 +0530

    solved

--- 104.Binary Tree Inorder Traversal.java
+++ 104.Binary Tree Inorder Traversal.java
@@ -1,15 +1,14 @@
 class Solution {
-    ArrayList<Integer> a=new ArrayList<>();
+    ArrayList<Integer> ans=new ArrayList<>();
   public List<Integer> inorderTraversal(TreeNode root) {
-      
       if(root==null){
           return new ArrayList<>();
       }
       else{
       inorderTraversal(root.left);
-      a.add(root.val);
+      ans.add(root.val);
       inorderTraversal(root.right);
-      return a; 
+      return ans; 
       }  
   }
 }
